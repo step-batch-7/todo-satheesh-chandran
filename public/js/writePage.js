5b97e089f9c55c8313b9cdc19f81c922532c5020
Satheesh C <satheesancchandran@gmail.com>
2020-02-03 15:49:39 +0530

    Parsed the added items
    On the write page we  can add any item and mark it as done or undone
    When submitting the whole added items will be parsed and ready to send
    post request

---
+++ public/js/writePage.js
@@ -0,0 +1,54 @@
+const makeVisible = function() {
+  const inputBar = document.querySelector('#item-bar');
+  inputBar.style.visibility = 'visible';
+};
+
+const getTaskChildren = function(value) {
+  const checkbox = document.createElement('input');
+  const status = document.createElement('td');
+  const task = document.createElement('td');
+  const tableRow = document.createElement('tr');
+
+  checkbox.type = 'checkbox';
+  status.appendChild(checkbox);
+  task.innerText = value;
+  tableRow.appendChild(status);
+  tableRow.appendChild(task);
+  return tableRow;
+};
+
+const addTasks = function(event) {
+  const target = event.currentTarget;
+  if (event.key === 'Enter') {
+    const value = target.value;
+    target.value = '';
+    const tableRow = getTaskChildren(value);
+    tableRow.setAttribute('id', Date.now());
+    document.querySelector('#list-table').appendChild(tableRow);
+    document.querySelector('#item-bar').style.visibility = 'hidden';
+  }
+};
+
+const parseLists = function(task) {
+  const list = {};
+  list.id = task.getAttribute('id');
+  const children = Array.from(task.children);
+  const status = children[0].children[0].checked;
+  list.status = status;
+  list.name = children[1].innerText;
+  return list;
+};
+
+const submitLists = function() {
+  const items = Array.from(document.querySelector('#list-table').children);
+  items.shift();
+  const tasks = Array.from(items.map(parseLists));
+  const title = document.querySelector('#title-bar').value;
+  document.querySelector('#title-bar').value = '';
+  const toDoList = {
+    tasks: tasks,
+    name: title,
+    id: Math.floor(Math.random() * 10000000)
+  };
+  // console.log(JSON.stringify(toDoList));
+};
