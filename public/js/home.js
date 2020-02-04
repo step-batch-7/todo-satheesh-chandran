5f32e4ebb91fbe673c5bbed6e1d79e64250d07c9
Satheesh C <satheesancchandran@gmail.com>
2020-02-04 10:32:36 +0530

    deletion of the whole todo list added
    sending a post request from the client with content
    id of that selected list and filtering on the json conent

--- public/js/home.js
+++ public/js/home.js
@@ -1,10 +1,10 @@
 const createInnerHTML = function(name) {
   return `<td>${name}</td>
-            <td>
-              <button>Delete</button>&emsp;<a href="./editPage.html"
-                ><button>Edit</button></a
-              >
-            </td>`;
+  <td>
+  <a href="./home.html">
+  <button onclick="deleteList(event)">Delete</button></a>&emsp;
+  <a href="./editPage.html"><button>Edit</button></a>
+  </td>`;
 };
 
 const appendChild = function(list) {
@@ -22,4 +22,18 @@ const sendGetRequest = function() {
   };
   req.open('GET', '/list');
   req.send();
+};
+
+const sendPostRequest = function(url, content) {
+  const req = new XMLHttpRequest();
+  req.open('POST', url);
+  req.send(content);
+};
+
+const deleteList = function(event) {
+  const target = event.currentTarget;
+  const parent = target.parentElement.parentElement.parentElement;
+  const id = parent.getAttribute('id');
+  sendPostRequest('/delete', id);
+  sendGetRequest();
 };
