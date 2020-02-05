35faac781466a8656dbec3246237b9ac1aafed88
Satheesh C <satheesancchandran@gmail.com>
2020-02-05 19:03:26 +0530

    Showed the existing elements on the edit Page

--- public/js/editPage.js
+++ public/js/editPage.js
@@ -1,12 +1,35 @@
-// const requestForJSON = function() {};
-
-const requestForId = function() {
+const requestForClickedElement = function(url, callback) {
   const req = new XMLHttpRequest();
   req.onload = function() {
-    console.log(this.responseText);
+    callback(JSON.parse(this.responseText));
   };
-  req.open('GET', '/tasks');
+  req.open('GET', url);
   req.send();
 };
 
-window.onload = requestForId;
+const getTableRowTemplate = function({ name, id, status }) {
+  const toTdElements = () => `<td><input type="checkbox"/></td>
+  <td>${name}</td><td><button>delete</button></td>`;
+
+  const tableRow = document.createElement('tr');
+  tableRow.innerHTML = toTdElements();
+  tableRow.querySelector('td input').checked = status;
+  document.querySelector('#list-table tbody').appendChild(tableRow);
+};
+
+const appendTableRow = function(element) {
+  document.querySelector('body p').innerText = element.name;
+  const tasks = element.tasks;
+  tasks.forEach(getTableRowTemplate);
+};
+
+const main = function() {
+  requestForClickedElement('/tasks', appendTableRow);
+};
+window.onload = main;
+
+// `<tr>
+//  <td><input type="checkbox" /></td>
+//  <td>sruthy</td>
+//  <td><button>delete</button></td>
+//  </tr>`;
