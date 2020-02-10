d82e1c0d8df493b787cd9399314f150b3a3de3f4
Satheesh C <satheesancchandran@gmail.com>
2020-02-10 11:08:52 +0530

    Design of login index.html

---
+++ public/js/index.js
@@ -0,0 +1,4 @@
+const toggleWindow = function() {
+  document.querySelector('#signup-window').classList.remove('invisible');
+  document.querySelector('#login-window').classList.add('invisible');
+};
