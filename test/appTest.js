8c00d3699caec1ba484b292552321f0f37095456
Satheesh C <satheesancchandran@gmail.com>
2020-02-11 09:25:55 +0530

    added a test for getting home.css

--- test/appTest.js
+++ test/appTest.js
@@ -6,11 +6,19 @@ const { app } = require('../lib/handlers');
 const STATUS_CODES = { OK: 200, METHOD_NOT_FOUND: 400, REDIRECT: 301 };
 
 describe('GET request for static files', function() {
-  it('should give the home page with url /', done => {
+  it('should give the home.html for url /', done => {
     request(app.serve.bind(app))
       .get('/')
       .expect('Content-Type', 'text/html')
       .expect(/\/css\/home.css/)
+      .expect(STATUS_CODES.OK, done);
+  });
+
+  it('should give the home.css url /home.css', done => {
+    request(app.serve.bind(app))
+      .get('/css/home.css')
+      .expect('Content-Type', 'text/css')
+      .expect(/body {/)
       .expect(STATUS_CODES.OK, done);
   });
 
