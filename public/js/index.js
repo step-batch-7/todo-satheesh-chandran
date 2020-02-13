1ae0be8f81e93a4fac73e8bde31e9ddd720c1e76
Satheesh C <satheesancchandran@gmail.com>
2020-02-13 12:14:59 +0530

    [satheesh | abhilash] Styled the UI of login page.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,9 @@
 const toggleWindow = function() {
-  document.querySelector('#signup-window').classList.remove('invisible');
-  document.querySelector('#login-window').classList.add('invisible');
+  if (event.target.innerHTML === 'Sign Up') {
+    document.querySelector('.signup-window').classList.remove('invisible');
+    document.querySelector('.login-window').classList.add('invisible');
+    return;
+  }
+  document.querySelector('.signup-window').classList.add('invisible');
+  document.querySelector('.login-window').classList.remove('invisible');
 };
