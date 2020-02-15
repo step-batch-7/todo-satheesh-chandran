3c2639612dcb6a81ef786e59b1edf98ebaaae8ef
Satheesh C <satheesancchandran@gmail.com>
2020-02-15 16:42:28 +0530

    [satheesh | abhilash] added signup handler.

--- config.js
+++ config.js
@@ -2,5 +2,6 @@ const { env } = process;
 
 module.exports = {
   DATA_STORE: `${__dirname}/${env.DATA_STORE}`,
-  SESSION_STORE: `${__dirname}/${env.SESSION_STORE}`
+  SESSION_STORE: `${__dirname}/${env.SESSION_STORE}`,
+  CREDENTIALS_STORE: `${__dirname}/${env.CREDENTIALS_STORE}`
 };
