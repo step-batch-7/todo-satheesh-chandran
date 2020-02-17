1cd516b8c26c0ed7d304a218b5e1d7a2384835f9
Satheesh C <satheesancchandran@gmail.com>
2020-02-17 09:33:09 +0530

    [satheesh | abhilash] removed writing session details.

--- config.js
+++ config.js
@@ -2,6 +2,5 @@ const { env } = process;
 
 module.exports = {
   DATA_STORE: `${__dirname}/${env.DATA_STORE}`,
-  SESSION_STORE: `${__dirname}/${env.SESSION_STORE}`,
   CREDENTIALS_STORE: `${__dirname}/${env.CREDENTIALS_STORE}`
 };
