66db883226b174787be6115fa20af8892c6ea317
Satheesh C <satheesancchandran@gmail.com>
2020-02-20 12:20:24 +0530

    [satheesh | abhilash] change path in package.json

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const { env } = process;
 
 module.exports = {
-  DATA_STORE: `${__dirname}/${env.DATA_STORE}`,
-  CREDENTIALS_STORE: `${__dirname}/${env.CREDENTIALS_STORE}`
+  DATA_STORE: env.DATA_STORE,
+  CREDENTIALS_STORE: env.CREDENTIALS_STORE
 };
