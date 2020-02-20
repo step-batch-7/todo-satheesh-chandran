242c2b8f3024cce67bd5339715fe6dda1ec79a89
Satheesh C <satheesancchandran@gmail.com>
2020-02-20 11:56:07 +0530

    [satheesh | abhilash] Changed PORT number configurable.

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 const { app } = require('./lib/routes');
 
-const port = 8000;
+const PORT = process.env.PORT || 8000;
 
 const main = function() {
-  app.listen(port, () => process.stdout.write(`Listening at ${port}`));
+  app.listen(PORT, () => process.stdout.write(`Listening at ${PORT}`));
 };
 
 main();
