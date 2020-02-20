2ac8e600328c8cf627709a9c6860ce0fef8485a7
Satheesh C <satheesancchandran@gmail.com>
2020-02-20 12:38:48 +0530

    [satheesh | abhilash] checking whether the data folder is present.

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const { app } = require('./lib/routes');
+const { createDataDir } = require('./lib/fileSystems');
 
 const PORT = process.env.PORT || 8000;
 
 const main = function() {
+  createDataDir('data');
   app.listen(PORT, () => process.stdout.write(`Listening at ${PORT}`));
 };
 
